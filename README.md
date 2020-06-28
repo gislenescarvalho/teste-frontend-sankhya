df25e8654e7f62fdee468e8d9839c035a9c23cac
Gislene Carvalho <gislene.dossantos.carvalho@outlook.com>
2020-06-28 18:44:22 -0300

    Fix: Optimize Fibonacci calc

--- README.md
+++ README.md
@@ -1,20 +1,28 @@
 # Teste frontend sankhya
 
 Ao abrir o arquivo index.html no navegador podemos ver que existe um timer que mostra a data e hora atual, inclusive segundos.
-Nessa mesma tela o usuário pode efetuar o cálculo de Fibonacci do número 42, porém, ao clicar no botão Calcular Fibonacci podemos perceber que o timer fica travado até que o cálculo seja finalizado, ao finalizar o timer volta ser executado normalmente. 
+Nessa mesma tela o usuário pode efetuar o cálculo de Fibonacci do número 42, porém, ao clicar no botão Calcular Fibonacci podemos perceber que o timer fica travado até que o cálculo seja finalizado, ao finalizar o timer volta ser executado normalmente.
 
 - O objetivo desse teste é fazer com que o timer não fique travado até que o cálculo de fibonacci seja finalizado, você deve:
   - Apresentar uma explicação do porque esse problema ocorre.
   - Apresentar uma ou mais possíveis soluções para o problema.
   - Implementar uma das soluções propostas.
- 
+
 Para realizar o teste, faça um fork do projeto, descreva o motivo do problema e as soluções propostas neste arquivo logo abaixo :arrow_down:
 
 # Causa do problema:
-Descreva aqui a causa do problema.
+
+Ao analisar aonde acontecia o gargalo na aplicação verifiquei que o clicar no botão de calcular o fibonacci de 42 o método de cálculo
+levava alguns segundos para retornar o resultado e liberar a execução novamente a função do setTimeout que faz a parte da função de timer.
 
 # Soluções:
+
 Descreva aqui as soluções do problema, destaque a que você escolheu para ser implementada.
+
+Uma técnica de otimização de execução de funções é a chamada
+Memoization(Memoisation) que armazena os resultados previamente calculados ou de cada chamada da função afim de otimizar a execução.
+
+Para solução do problema escolhi essa técnica, pois a mesma diminue a complexidade da função a um nível linear ou constante. Utilizei um tipo Object para armazenar os valores já calculados previamente. O tempo de execução passa de segundos para milissegundos conforme pode ser visto na execução da aplicação.
 
 Ao iniciar o teste, envie um e-mail para seu entrevistador informando o prazo de conclusão do teste.
 Ao finalizar o teste encaminhe o link do fork com a implementação para o email do seu entrevistador.
--- src/app.js
+++ src/app.js
@@ -1,44 +1,59 @@
-const MAP_MONTH = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'];
+const MAP_MONTH = [
+	"Jan",
+	"Fev",
+	"Mar",
+	"Abr",
+	"Mai",
+	"Jun",
+	"Jul",
+	"Ago",
+	"Set",
+	"Out",
+	"Nov",
+	"Dez",
+];
 
-window.onload = function(){
+window.onload = function () {
+	var x = setInterval(function () {
+		let now = new Date();
 
-    var x = setInterval(function() {
-        
-        let now = new Date();
+		let day = now.getDate().toString().padStart(0, 2);
 
-        let day = now.getDate().toString().padStart(0, 2);
-        
-        let month = MAP_MONTH[now.getMonth()];
+		let month = MAP_MONTH[now.getMonth()];
 
-        let year = now.getFullYear();
+		let year = now.getFullYear();
 
-        let hour = now.getHours().toString().padStart(2, '0');
-        
-        let minute = now.getMinutes().toString().padStart(2, '0');
-        
-        let seconds = now.getSeconds().toString().padStart(2, '0');
-        
-        document.getElementById("timer-content").innerHTML = `${day} de ${month} de ${year} ${hour}:${minute}:${seconds}`;
+		let hour = now.getHours().toString().padStart(2, "0");
 
-    }, 1000);
+		let minute = now.getMinutes().toString().padStart(2, "0");
 
+		let seconds = now.getSeconds().toString().padStart(2, "0");
+
+		document.getElementById(
+			"timer-content"
+		).innerHTML = `${day} de ${month} de ${year} ${hour}:${minute}:${seconds}`;
+	}, 1000);
+};
+
+function calc(n) {
+	var start = new Date().getTime();
+	var fibonacciCalculated = fibonacci(n);
+	var end = new Date().getTime();
+	var time = end - start;
+
+	let elResult = document.getElementById("fibonacci-result");
+	elResult.innerText = `Fibonacci de ${n}: ${fibonacciCalculated} / Tempo gasto: ${time} ms`;
 }
 
-function calc(n){
-    let elResult = document.getElementById('fibonacci-result');
-    elResult.innerText = `Fibonacci de ${n}: ${fibonacci(n)}`;
-}
+// Uma tática de otimização é armazenar os resultados
+// dos cálculos previamente calculados em um tipo Object.
 
-
-function fibonacci(n){
-    if (n === 0) {
-        return 0;
-    }
-    if (n === 1) {
-        return 1;
-    }
-    else {
-        return fibonacci(n - 1) +
-            fibonacci(n - 2);
-    }
+var nums = {};
+function fibonacci(n) {
+	var acc;
+	if (n < 3) return 1;
+	else if (nums.hasOwnProperty(n)) acc = nums[n];
+	else acc = fibonacci(n - 2) + fibonacci(n - 1);
+	nums[n] = acc;
+	return acc;
 }
